16e109df0d8926cea5acd838779f844c7ec2af68
joshuaonwezen <joshua.onwezen@gmail.com>
2024-10-01 08:53:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ apps/frontend/src/components/cms/element/index.ts
@@ -0,0 +1,44 @@
+// Auto generated dictionary
+import { ComponentTypeDictionary } from "@remkoj/optimizely-cms-react";
+import ArticleListElementComponent from "./ArticleListElement";
+import ContentRecsElementComponent from "./ContentRecsElement";
+import CTAElementComponent from "./CTAElement";
+import HeadingElementComponent from "./HeadingElement";
+import ImageElementComponent from "./ImageElement";
+import ParagraphElementComponent from "./ParagraphElement";
+import TestimonialElementComponent from "./TestimonialElement";
+
+// Build dictionary
+export const elementDictionary : ComponentTypeDictionary = [
+    { 
+        type: "ArticleListElement", 
+        component: ArticleListElementComponent 
+    },
+    { 
+        type: "ContentRecsElement", 
+        component: ContentRecsElementComponent 
+    },
+    { 
+        type: "CTAElement", 
+        component: CTAElementComponent 
+    },
+    { 
+        type: "HeadingElement", 
+        component: HeadingElementComponent 
+    },
+    { 
+        type: "ImageElement", 
+        component: ImageElementComponent 
+    },
+    { 
+        type: "ParagraphElement", 
+        component: ParagraphElementComponent 
+    },
+    { 
+        type: "TestimonialElement", 
+        component: TestimonialElementComponent 
+    }
+];
+
+// Export dictionary
+export default elementDictionary;
